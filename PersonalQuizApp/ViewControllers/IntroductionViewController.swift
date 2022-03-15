ae88902e17b33ed3079f8559a4de36fc56f5950a
Alex Rybachev <rybachev.as@gmail.com>
2022-03-15 12:35:43 +0400

    add classes files for VCs

--- PersonalQuizApp/ViewControllers/IntroductionViewController.swift
+++ PersonalQuizApp/ViewControllers/IntroductionViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  IntroductionViewController.swift
 //  PersonalQuizApp
 //
 //  Created by Aleksandr Rybachev on 15.03.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class IntroductionViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
