134e1063fddb6dbca644ecee21533ba5a6476eee
Alex Rybachev <rybachev.as@gmail.com>
2022-03-15 12:47:26 +0400

    add outlets

--- PersonalQuizApp/ViewControllers/IntroductionViewController.swift
+++ PersonalQuizApp/ViewControllers/IntroductionViewController.swift
@@ -9,11 +9,7 @@ import UIKit
 
 class IntroductionViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    @IBAction func unwind(for segue: UIStoryboardSegue) {}
 
 }
 
--- PersonalQuizApp/ViewControllers/QuestionsViewController.swift
+++ PersonalQuizApp/ViewControllers/QuestionsViewController.swift
@@ -8,6 +8,23 @@
 import UIKit
 
 class QuestionsViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var questionProgressView: UIProgressView!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet var rangedSlider: UISlider!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
